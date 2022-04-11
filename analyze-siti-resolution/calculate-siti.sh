d80fb295a6dd076d59df11dfaf80ce6f43673221
Werner Robitza <werner.robitza@gmail.com>
2022-04-11 12:47:22 +0200

    fix for renaming data

--- analyze-siti-resolution/calculate-siti.sh
+++ analyze-siti-resolution/calculate-siti.sh
@@ -14,9 +14,7 @@ fi
 find "$1" -name '*h264.mp4' -print0 | parallel --eta --progress -0 'siti --full-range -q -of csv {} > data/{/.}.csv'
 
 # regularize data names
-rename 's/_(\d+)kbps_/-$1-/' data/*
-rename 's/_(\d+)p_/-$1-/' data/*
-rename 's/_(\d+)fps_/-$1-/' data/*
-rename 's/_h264/-h264/' data/*
+rename 's/_(\d+)kbps_(\d+)p_([\d\.]+)fps_(\w+)/-$1-$2-$3-$4/' data/*
+rename 's/LeagueOfLegends-1/LeagueOfLegends/' data/*
 
 echo "Calculation done"
