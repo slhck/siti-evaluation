3def54840189ca7471aadc57c6587e052717ae60
Werner Robitza <werner.robitza@gmail.com>
2022-05-13 20:42:24 +0200

    prep for VQEG HDTV dataset

--- siti-plots/README.md
+++ siti-plots/README.md
@@ -6,6 +6,12 @@ Examples of how SI/TI can be plotted.
 
 - Rstudio
 
+## Data
+
+Here, the VQEG HDTV Phase 1 dataset `vqeghdtv1` was used. It can be downloaded from https://cdvl.org.
+
+To re-analyze the data, run the `analyze.sh` script. You need `jq` and GNU `parallel` for that.
+
 ## Usage
 
 Run the `analyze.R` script.
---
+++ siti-plots/analyze.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")" || exit 1
+
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <path-to-VQEG-HDTV-videos>"
+  exit 1
+fi
+
+# to unzip:
+# 7z x VQEG_HDTV_1.zip 'vqegHD1/*_original.avi'
+
+parallel --jobs 1 --eta --progress 'ffmpeg -i {} -c:v rawvideo -pix_fmt yuv420p {.}-420p.avi' ::: "$1"/*.avi
+
+parallel --tag --eta 'siti-tools --color-range=full {} > {.}-siti.json' ::: "$1/"*420p.avi
+
